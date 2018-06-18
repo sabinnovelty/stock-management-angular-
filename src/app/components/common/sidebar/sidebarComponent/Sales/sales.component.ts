b0c1ac4f761db4e8eacab7c9831708f5363039a1
Saigal <saigal.amatya@gmail.com>
2018-06-18 14:01:33 +0545

    added unstaged files

---
+++ src/app/components/common/sidebar/sidebarComponent/Sales/sales.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salesList',
+  templateUrl: 'sales.component.html',
+  styleUrls: ['./sales.component.style.css']
+})
+
+export class SalesListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
